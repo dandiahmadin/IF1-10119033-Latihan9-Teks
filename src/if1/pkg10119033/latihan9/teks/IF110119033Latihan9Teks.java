2e63b63f884ac8a23b2f027abab93d9754373944
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 10:45:02 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan9/teks/IF110119033Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi jenis tipe data teks menggunakan char
+ */
+public class IF110119033Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + "berisi " + c);
+    }
+    
+}
